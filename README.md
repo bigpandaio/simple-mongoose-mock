b911803fdb7e3f18f9a3b8caa90a2dcce2d46a5a
bobrowadam <adambobrow@Adams-MacBook-Pro.local>
2017-12-24 19:42:39 +0200

    Add simple.mongoose.mock

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # simple-mongoose-mock
-A simplified mongoose mock
+
+A simplified mongoose mock.
+
+## Usage Example
+
+#### User.js
+```JavaScript
+var { Schema } = require('mongoose');
+
+var User = new Schema({});
+
+
+module.exports = mongoose.model('User', User);
+```
+#### User.spec.js
+```JavaScript
+const User = require('User');
+const sinon = require('sinon');
+const mongooseMockModel = require('simple-mongoose-mock');
+
+const proxyquire = require('proxyquire');
+const mongooseMockModel = require('bp-api-common/lib/test-utils/mongoose.mock');
+
+const userExecStub = sinon.stub();
+const userStubedModel = mongooseMockModel('User', { exec: userExecStub });
+
+userExecStub.returns({ name: 'bob', age: 4});
+
+expect(userStubedModel
+       .find()
+       .exec()).to.equal({ name: 'bob', age: 4});
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = (modelName, stubbedMethods) => {
+  return modelGen(modelName, stubbedMethods);
+};
+
+function modelGen(stubbedModelName, stubbedMethods) {
+  return defaultMethods.reduce((modelObj, method) =>
+    stubbedMethods[method] ?
+    Object.assign(modelObj, { [method]: stubbedMethods[method] }) :
+    Object.assign(modelObj, { [method]: () => modelObj }), {});
+}
+
+const defaultMethods = [
+  'aggregate',
+  'count',
+  'create',
+  'distinct',
+  'ensureIndexes',
+  'find',
+  'findById',
+  'findByIdAndRemove',
+  'findByIdAndUpdate',
+  'findOne',
+  'findOneAndRemove',
+  'findOneAndUpdate',
+  'geoNear',
+  'geoSearch',
+  'index',
+  'mapReduce',
+  'lean',
+  'exec',
+  'plugin',
+  'populate',
+  'remove',
+  'set',
+  'update',
+  'where',
+  'toObject',
+  'toString',
+  'updateMany',
+  'select',
+  'add',
+  'sort'
+];
